569067064cba3c012613c1a6ccd73ad87ade7cc3
Bill Wong <bill.wong@lansa.com>
2016-08-09 18:53:51 -0400

    Adding players to the state

--- src/Main.js
+++ src/Main.js
@@ -8,7 +8,8 @@ class Main extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			teams: []
+			teams: [],
+			players: []
 		}
 	}
 
@@ -16,17 +17,38 @@ class Main extends Component {
 		fetch(teamDataURL, { method: 'get' })
 			.then(response => response.json())
 			.then(responseJson => {
-				const dropdownArray = responseJson.map(val => ({
-					value: val.id,
-					label: val.teamName
-				}));
-				this.setState({
-					teams: dropdownArray
+
+				// [TODO] Clear state of teams and players?
+
+				// Map the response into a teams array -- set the state; used to build dropdown
+				const teamsArray = responseJson.map(val => {
+
+					// Map each player in each team into a players array -- set the state; used to build roster
+					const playerArray = val.players.map(p => ({
+						teamName: val.teamName,
+						gender: p.gender,
+						playerName: p.playerName,
+						nickname: p.nickname
+					}));
+
+					// Concatenate players from each team to the state
+					this.setState({
+						players: this.state.players.concat(playerArray)
+					});
+					
+					// Returning the team data
+					return {	value: val.id,	label: val.teamName	}
 				});
+
+				this.setState({
+					teams: teamsArray
+				});
+
 			})
 			.catch(err => {
 				console.log(err);
 			});
+
 	}
 
 	teamChanged(teamObject) {
