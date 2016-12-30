31561aa07105b8872575cf482a29b674664f5dbe
Bill Wong <b6wong@gmail.com>
2016-12-29 20:03:35 -0500

    Updated stats table to separate gender passing data.

--- src/components/GameTeamStats.js
+++ src/components/GameTeamStats.js
@@ -21,31 +21,35 @@ function ShowScorebord(props) {
   const {statisticsData} = props;
 
   return (
-    <Table responsive striped condensed>
+    <Table responsive striped condensed className="table-stats">
         <thead>
             <tr>
-            <th>Player</th>
-            <th>G</th>
-            <th>A</th>
-            <th>2A</th>
-            <th>D!</th>
-            <th>Drop</th>
-            <th>TA</th>
-            <th>Pass</th>
+                <th className="player-name">Player</th>
+                <th>G</th>
+                <th>A</th>
+                <th>2A</th>
+                <th>D!</th>
+                <th>Drop</th>
+                <th>TA</th>
+                <th>Pass</th>
+                <th>Pass (M)</th>
+                <th>Pass (F)</th>
             </tr>
         </thead>
         <tbody>
             {
             statisticsData.map(player =>
                 <tr key={player.id}>
-                <td>{player.nickname}</td>
-                <td>{player.statGoal}</td>
-                <td>{player.statAssist}</td>
-                <td>{player.stat2Assist}</td>
-                <td>{player.statD}</td>
-                <td>{player.statDrop}</td>
-                <td>{player.statTA}</td>
-                <td>{player.statsPass} ({player.statsPassMale}:{player.statsPassFemale})</td>    
+                    <td className="player-name">{player.nickname}</td>
+                    <td>{player.statGoal}</td>
+                    <td>{player.statAssist}</td>
+                    <td>{player.stat2Assist}</td>
+                    <td>{player.statD}</td>
+                    <td>{player.statDrop}</td>
+                    <td>{player.statTA}</td>
+                    <td>{player.statsPass}</td>    
+                    <td>{player.statsPassMale}</td>
+                    <td>{player.statsPassFemale}</td>
                 </tr>
             )
             }
