f9cc1cf2a36fdcc323f16e812d0fda3c74cfaf0e
Colin Thompson <thompson.colin@icloud.com>
2021-09-28 08:52:05 -0400

    Update for 2021 fall indoor season

--- src/stores/leagueStore/index.js
+++ src/stores/leagueStore/index.js
@@ -9,7 +9,7 @@ class LeagueStore {
     @observable playersList;
 
     constructor() {
-        this.leagueId = 652;
+        this.leagueId = 694;
         this.gamesList = [];
         this.teamsList = [];
         this.playersList = [];
