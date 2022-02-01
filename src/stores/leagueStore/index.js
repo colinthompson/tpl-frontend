c56af0075949570246d729067a80619c53663926
Colin Thompson <thompson.colin@icloud.com>
2022-02-01 09:24:22 -0500

    Update for 2022 winter indoor season

--- src/stores/leagueStore/index.js
+++ src/stores/leagueStore/index.js
@@ -9,7 +9,7 @@ class LeagueStore {
     @observable playersList;
 
     constructor() {
-        this.leagueId = 694;
+        this.leagueId = 703;
         this.gamesList = [];
         this.teamsList = [];
         this.playersList = [];
