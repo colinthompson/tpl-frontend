3a6942fc8ff79787b3b01907d0e587c6848a39f5
Colin Thompson <thompson.colin@icloud.com>
2022-09-05 10:40:06 -0400

    Change backend URL
    Heroku is discontinuing their free tier, so the backend has been moved to Render.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
 const isDev = process.env.NODE_ENV === 'development';
 
-export const SERVER_URI = '//tplstats.herokuapp.com/'; // Production server
+export const SERVER_URI = 'https://tplapp.onrender.com/'; // Production server
 // export const SERVER_URI = '//localhost:3001/'; // Development server
